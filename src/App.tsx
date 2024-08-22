aacb27b1392db10bd4413419784b63b77aab6de2
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-22 12:05:50 +0200

    ✨ HomePage : adding errorPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/pages/home/HomePage";
+import ErrorPage from "./components/pages/error/ErrorPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<ErrorPage />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
